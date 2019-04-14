b738f2a184fb210fe5acc820b8be5bcc0409b980
Aerobautics <48452349+Aerobautics@users.noreply.github.com>
2019-04-14 08:33:17 -0700

    Modified launch scripts
    Modified launch scripts to get stuff operational

--- first_challenge_quad_launcher.sh
+++ first_challenge_quad_launcher.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd ~/catkin_ws/src/Firmware/
+source /opt/ros/kinetic/setup.bash 
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+
 cd ~/catkin_ws
 source /opt/ros/kinetic/setup.bash
 source ./devel/setup.bash
--- first_challenge_world_launcher.sh
+++ first_challenge_world_launcher.sh
@@ -3,4 +3,5 @@ cd ~/catkin_ws
 source /opt/ros/kinetic/setup.bash
 source ./devel/setup.bash
 source ./src/ai_battle_drone/scripts/setup_ai_drone.sh
+export GAZEBO_PLUGIN_PATH="/home/stewart/catkin_ws/src/Firmware/build/px4_sitl_default/build_gazebo"
 roslaunch ai_battle_drone challenge_1_world.launch
